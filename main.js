44a758750fed1dcf71933c4ee98ea88685a20a54
takwa202 <takwa.lassoued@esprit.tn>
2023-11-02 12:16:21 +0100

    fc

---
+++ main.js
@@ -0,0 +1,55 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your code here
+    console.log('hi');
+// Get references to your elements
+const section1 = document.querySelector('.section1');
+const s1 = document.getElementById('s1');
+const s2 = document.getElementById('s2');
+const s3 = document.getElementById('s3');
+const s4 = document.getElementById('s4');
+const s5 = document.getElementById('s5');
+const s6 = document.getElementById('s6');
+const s7 = document.getElementById('s7');
+const s8 = document.getElementById('s8');
+const s9 = document.getElementById('s9');
+
+// Add event listeners for hover
+s1.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im2.jpg')";
+});
+
+s2.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im.jpg')";
+});
+
+s3.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im.jpg')";
+});
+
+s4.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im2.jpg')";
+});
+
+s5.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im.jpg')";
+});
+
+s6.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im2.jpg')";
+});
+
+s7.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im2.jpg')";
+});
+
+s8.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im.jpg')";
+});
+
+s9.addEventListener('mouseenter', () => {
+  section1.style.backgroundImage = "url('img/im.jpg')";
+});
+
+  });
+  
